3b4cde3c37c19f507f35f7348e087c6ee3aff2a0
jeffch19 <17jcummings@gmail.com>
2024-04-25 14:38:42 -0400

    adding to program.cs

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -1,35 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 
-<string, double>
-<int, string>
-
-Dictionary<string, int> myScoreBoard = new Dictionary<string, int>();
-
-myScoreBoard.Add("firstInning", 10);
-myScoreBoard.Add("secondInning", 20);
-myScoreBoard.Add("thirdInning", 30);
-myScoreBoard.Add("fourthInning", 40);
-myScoreBoard.Add("fifthInning", 50);
-
 namespace CatWorx.BadgeMaker
 {
-  class Program
+class Program
+{
+  static List<string> GetEmployees()
   {
-    static void Main(string[] args)
+    List<string> employees = new List<string>();
+    while (true)
     {
-     Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, *
-Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3
-Console.WriteLine(1 + 2 * 3);     // order of operations
-Console.WriteLine(10 / 3.0);      // int's and doubles
-Console.WriteLine(10 / 3);        // int's 
-Console.WriteLine("12" + "3");    // What happens here?
 
-int num = 10;
-num += 100;
-Console.WriteLine(num);
-num ++;
-Console.WriteLine(num);
+      Console.WriteLine("Please enter a name: (leave empty to exit): ");
+
+      string input = Console.ReadLine() ?? "";
+
+      if (input == "")
+      {
+        break;
+      }
+      employees.Add(input);
     }
+    return employees;
+  }
+
+  static void PrintEmployees(List<string> employees)
+  {
+    for (int i = 0; i < employees.Count; i++)
+    {
+      Console.WriteLine(employees[i]);
+    }
+  }
+
+  static void Main(string[] args)
+  {
+    List<string> employees = GetEmployees();
+    PrintEmployees(employees);
   }
 }
