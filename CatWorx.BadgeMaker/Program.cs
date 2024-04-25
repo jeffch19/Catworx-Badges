17e868964b0eeba629d442da2bcf57d191ac8f37
jeffch19 <17jcummings@gmail.com>
2024-04-25 15:38:10 -0400

    successfully create employee instances

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -3,38 +3,45 @@ using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
-class Program
-{
-  static List<string> GetEmployees()
-  {
-    List<string> employees = new List<string>();
-    while (true)
+    class Program
     {
+        static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+                Console.WriteLine("Enter first name (leave empty to exit): ");
+                string firstName = Console.ReadLine() ?? "";
+                if (firstName == "")
+                {
+                    break;
+                }
 
-      Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine() ?? "";
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                Console.Write("Enter Photo URL:");
+                string photoUrl = Console.ReadLine() ?? "";
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+            }
 
-      string input = Console.ReadLine() ?? "";
+            return employees;
+        }
 
-      if (input == "")
-      {
-        break;
-      }
-      employees.Add(input);
+        static void PrintEmployees(List<Employee> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i].GetFullName());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Employee> employees = GetEmployees();
+            PrintEmployees(employees);
+        }
     }
-    return employees;
-  }
-
-  static void PrintEmployees(List<string> employees)
-  {
-    for (int i = 0; i < employees.Count; i++)
-    {
-      Console.WriteLine(employees[i]);
-    }
-  }
-
-  static void Main(string[] args)
-  {
-    List<string> employees = GetEmployees();
-    PrintEmployees(employees);
-  }
 }
